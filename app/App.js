b86abe0af3f6fd5d8f39a703b08a2ec905a49438
Davi Camarinha <davi.camarinha@agenciaecan.com.br>
2017-12-18 17:13:48 -0200

    Primeiro commit

---
+++ app/App.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import css from '../styles/style.css'
+
+ReactDOM.render(<starwars /> , document.getElementById('app'));
---
+++ app/components/planets.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default () => ( 
+    <div className = "container">
+		<h1>Teste</h1>
+		<div className="row">
+			<div className="col-md-2 bio">
+				<InfoUser />
+			</div>
+			<div className="col-md-8 tweets">
+				<TweetLine />
+			</div>
+			<div className="col-md-2 links">teste3</div>
+		</div>
+    </div>
+)
